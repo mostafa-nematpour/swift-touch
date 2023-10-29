5388e3b127df878ff996890bce81140cb0247c68
mostafa <nematpor46@gmail.com>
2023-10-29 12:21:56 +0330

    add click

--- src/SwiftTouch.js
+++ src/SwiftTouch.js
@@ -2,7 +2,10 @@ export default class SwiftTouch {
     st_option
     st_element
     baseOption = {
-        delay: 300
+        delay: 300,
+        checkTarget: false,
+        checkClickTarget: false,
+        checkTouchTarget: false,
     };
 
     constructor(element, option) {
@@ -14,9 +17,27 @@ export default class SwiftTouch {
         /*  touchstart >> touchscreen device
             mousedown  >> desktop     device
         */
-        const evtName = 'ontouchstart' in window ? "touchstart" : "mousedown";
-        this.st_element.addEventListener(evtName, (evt) => {
-            callBack(evt);
+        const eventName = 'ontouchstart' in window ? "touchstart" : "mousedown";
+        this.st_element.addEventListener(eventName, (event) => {
+            if (this.checkEventTarget(event, this.baseOption.checkTouchTarget)) {
+                event.stopPropagation();
+            } else {
+                callBack(event)
+            }
         });
+    }
+
+    click(callBack) {
+        this.st_element.addEventListener('click', (event) => {
+            if (this.checkEventTarget(event, this.baseOption.checkClickTarget)) {
+                event.stopPropagation();
+            } else {
+                callBack(event)
+            }
+        });
+    }
+
+    checkEventTarget(event, eventFlag = false) {
+        return (this.baseOption.checkTarget || eventFlag) && event.target !== event.currentTarget;
     }
 }
