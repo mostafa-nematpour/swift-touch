7aae6f7e38b2164962329c5a24c39d4493f27209
mostafa <nematpor46@gmail.com>
2023-10-30 21:33:12 +0330

    add touchMove

--- src/SwiftTouch.js
+++ src/SwiftTouch.js
@@ -6,6 +6,7 @@ export default class SwiftTouch {
         checkTarget: false,
         checkClickTarget: false,
         checkTouchTarget: false,
+        checkTouchMoveTarget: false,
     };
 
     constructor(element, option) {
@@ -19,25 +20,66 @@ export default class SwiftTouch {
         */
         const eventName = 'ontouchstart' in window ? "touchstart" : "mousedown";
         this.st_element.addEventListener(eventName, (event) => {
-            if (this.checkEventTarget(event, this.baseOption.checkTouchTarget)) {
+            if (this.#checkEventTarget(event, this.baseOption.checkTouchTarget)) {
                 event.stopPropagation();
             } else {
                 callBack(event)
             }
         });
+    }
+
+    touchMove(callBack) {
+        let isDragging = false;
+        let isMobile = 'ontouchstart' in window;
+        const data = { st_type: "touchMove" };
+
+        const innerTouchHandler = (event) => {
+            data.type = event.type;
+            const touchEventData = isMobile ? event.touches[0] || event.changedTouches[0] : event;
+            if (touchEventData) {
+                data.pageX = touchEventData.pageX;
+                data.pageY = touchEventData.pageY;
+                data.clientX = touchEventData.clientX;
+                data.clientY = touchEventData.clientY;
+                data.screenX = touchEventData.screenX;
+                data.screenY = touchEventData.screenY;
+            }
+            this.#touchMoveHandel(data, event, callBack);
+        }
+
+        if (isMobile) {
+            this.st_element.addEventListener('touchmove', innerTouchHandler);
+        } else {
+            this.st_element.addEventListener('mousedown', () => { isDragging = true });
+            this.st_element.addEventListener('mouseup', () => { isDragging = false });
+            this.st_element.addEventListener('mousemove', (event) => {
+                if (isDragging) {
+                    innerTouchHandler(event);
+                    event.preventDefault();
+                }
+            });
+        }
+    }
+
+    #touchMoveHandel(data, event, callBack) {
+        if (this.#checkEventTarget(event, this.baseOption.checkTouchMoveTarget)) {
+            event.stopPropagation();
+        } else {
+            callBack(data, event)
+        }
     }
 
     click(callBack) {
         this.st_element.addEventListener('click', (event) => {
-            if (this.checkEventTarget(event, this.baseOption.checkClickTarget)) {
+            if (this.#checkEventTarget(event, this.baseOption.checkClickTarget)) {
                 event.stopPropagation();
             } else {
-                callBack(event)
+                callBack(event);
             }
         });
     }
 
-    checkEventTarget(event, eventFlag = false) {
+    #checkEventTarget(event, eventFlag = false) {
         return (this.baseOption.checkTarget || eventFlag) && event.target !== event.currentTarget;
     }
 }
